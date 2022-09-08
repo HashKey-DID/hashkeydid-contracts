b93e72d10dd87df57759f4bada0127fedaeba956
quanrong <quanrong@hashquark.io>
2022-09-08 16:17:52 +0800

    format test file

--- test/DeedGrain.js
+++ test/DeedGrain.js
@@ -1,46 +1,51 @@
 const { expect } = require("chai");
-const { ethers } = require('hardhat');
+const { ethers } = require("hardhat");
 
+describe("DeedGrain", async () => {
+  let accounts;
+  let deployer;
+  let deedGrain;
 
-describe("DidGrain", async() => {
-    let accounts;
-    let deployer;
-    let didGrain;
-    before(async () => {
-        accounts = await ethers.getSigners();
-        deployer = accounts[0];
-    })
-    beforeEach(async () => {
-        const DidGrain = await ethers.getContractFactory("DeedGrain");
-        didGrain = await DidGrain.deploy("DidGrain","DG","",false);
-        await didGrain.deployed();
-    })
-    it("Should be not able to mint DG", async () => {
-        await expect(didGrain.connect(accounts[1]).mint(deployer.address,1)).to.be.revertedWith("");
-    })
-    it("Should be able to mint DG", async () => {
-        await didGrain.connect(accounts[0]).mint(deployer.address,1);
-        expect(await didGrain.balanceOf(deployer.address,1)).to.equal(1);
-    })
-    it("Should be not able to transfer DG", async () => {
-        await didGrain.mint(deployer.address,1);
-        await expect(didGrain.connect(deployer).safeTransferFrom(deployer.address,accounts[1].address,1,1,ethers.utils.randomBytes(0))).to.be.revertedWith(
-            'this NFT is not allowed to be transferred'
-            );
-    })
-    it("Should be able to transfer DG", async () => {
-        await didGrain.mint(deployer.address,1);
-        await didGrain.reverseTransferable();
-        await didGrain.connect(deployer).safeTransferFrom(deployer.address,accounts[1].address,1,1,ethers.utils.randomBytes(0));
-        expect(await didGrain.balanceOf(accounts[1].address,1)).to.equal(1);
-    })
-    it("Should be able to burn DG", async () => {
-        await didGrain.mint(deployer.address,1);
-        await didGrain.connect(deployer).burn(deployer.address,1,1);
-        expect(await didGrain.balanceOf(deployer.address,1)).to.equal(0);
-    })
-    it("Should be not able to burn DG", async () => {
-        await didGrain.mint(deployer.address,1);
-        await expect(didGrain.connect(accounts[1]).burn(deployer.address,1,1)).to.be.revertedWith("");
-    })
+  before(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+  });
+
+  beforeEach(async () => {
+    const DeedGrain = await ethers.getContractFactory("DeedGrain");
+    deedGrain = await DeedGrain.deploy("DeedGrain", "DG", "", false);
+    await deedGrain.deployed();
+  });
+
+  it("Should not be able to mint DG", async () => {
+    await expect(deedGrain.connect(accounts[1]).mint(deployer.address, 1)).to.be.revertedWith("");
+  });
+
+  it("Should be able to mint DG", async () => {
+    await deedGrain.connect(accounts[0]).mint(deployer.address, 1);
+    expect(await deedGrain.balanceOf(deployer.address, 1)).to.equal(1);
+  });
+
+  it("Should not be able to transfer DG", async () => {
+    await deedGrain.mint(deployer.address, 1);
+    await expect(deedGrain.connect(deployer).safeTransferFrom(deployer.address, accounts[1].address, 1, 1, ethers.utils.randomBytes(0))).to.be.revertedWith("this NFT is not allowed to be transferred");
+  });
+
+  it("Should be able to transfer DG", async () => {
+    await deedGrain.mint(deployer.address, 1);
+    await deedGrain.reverseTransferable();
+    await deedGrain.connect(deployer).safeTransferFrom(deployer.address, accounts[1].address, 1, 1, ethers.utils.randomBytes(0));
+    expect(await deedGrain.balanceOf(accounts[1].address, 1)).to.equal(1);
+  });
+
+  it("Should be able to burn DG", async () => {
+    await deedGrain.mint(deployer.address, 1);
+    await deedGrain.connect(deployer).burn(deployer.address, 1, 1);
+    expect(await deedGrain.balanceOf(deployer.address, 1)).to.equal(0);
+  });
+
+  it("Should not be able to burn DG", async () => {
+    await deedGrain.mint(deployer.address, 1);
+    await expect(deedGrain.connect(accounts[1]).burn(deployer.address, 1, 1)).to.be.revertedWith("");
+  });
 });
