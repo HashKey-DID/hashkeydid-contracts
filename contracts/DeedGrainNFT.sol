c6f6e7a5fefc2bbcc69bfa4796a604989bc5b935
icesfeathers <774001810@qq.com>
2023-03-31 18:20:37 +0800

    did assembly;

--- contracts/DeedGrainNFT.sol
+++ contracts/DeedGrainNFT.sol
@@ -4,16 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DeedGrainNFT is ERC721 {
-    event ControllerAdded(address indexed newController);
-    event ControllerRemoved(address indexed oldController);
-
-    modifier onlyControllers() {
-        require(msg.sender == issuer || msg.sender == controller);
-        _;
-    }
-
-    string private _deedgrainName;
-    string private _deedgrainSymbol;
     string private _baseMetadataURI;
     uint256 public totalSupply;
     uint256 public supply;
@@ -22,33 +12,57 @@ contract DeedGrainNFT is ERC721 {
     // tokenId to series id.
     mapping(uint256 => uint256) public sids;
 
+    modifier onlyControllers() {
+        // require(msg.sender == issuer || msg.sender == controller);
+        assembly{
+            if iszero(or(eq(caller(), sload(issuer.slot)), eq(caller(), sload(controller.slot)))){
+                revert(0, 0) //TODO
+            }
+        }
+        _;
+    }
+
     constructor(
         string memory _name,
         string memory _symbol,
         string memory _uri,
         uint256 _supply
-    ) ERC721("", "") {
-        _deedgrainName = _name;
-        _deedgrainSymbol = _symbol;
-        controller = msg.sender;
-        issuer = tx.origin;
-        supply = _supply;
+    ) ERC721(_name, _symbol) {
+        assembly{
+        // controller = msg.sender;
+        // issuer = tx.origin;
+        // supply = _supply;
+            sstore(controller.slot, caller())
+            sstore(issuer.slot, origin())
+            sstore(supply.slot, _supply)
+        }
+
         _baseMetadataURI = _uri;
     }
 
     function mint(address to, uint256 sid)
-        public
-        onlyControllers
-        returns (uint256)
+    public
+    onlyControllers
+    returns (uint256)
     {
-        if (supply != 0) {
-            require(totalSupply + 1 <= supply, "insufficient supply");
-        }
-        totalSupply++;
-        uint256 tokenId = totalSupply;
+        uint256 tokenId;
+        assembly{
+        // require(totalSupply + 1 <= supply, "insufficient supply");
+        // totalSupply++;
+        // uint256 tokenId = totalSupply;
+        // sids[tokenId] = sid;
+            tokenId := add(sload(totalSupply.slot), 1)
+            if gt(tokenId, sload(supply.slot)){
+                revert(0, 0) //TODO
+            }
+            sstore(totalSupply.slot, tokenId)
 
+            let s := mload(0x40)
+            mstore(s, tokenId)
+            mstore(add(s, 0x20), tokenId)
+            sstore(keccak256(s, 0x40), sid)
+        }
         _mint(to, tokenId);
-        sids[tokenId] = sid;
         return tokenId;
     }
 
@@ -56,16 +70,27 @@ contract DeedGrainNFT is ERC721 {
         address[] calldata addrs,
         uint256[] calldata seriesIds
     ) external onlyControllers {
-        require(addrs.length == seriesIds.length, "accounts length not equal to sidArr length");
-        if (supply != 0) {
-            require(totalSupply + addrs.length <= supply, "insufficient supply");
+        uint256 len;
+        assembly{
+        // require(addrs.length == seriesIds.length, "accounts length not equal to sidArr length");
+            len := mload(addrs.offset)
+            if iszero(eq(len, mload(seriesIds.offset))){
+                revert(0, 0) //TODO
+            }
+
+        // require(totalSupply + addrs.length <= supply, "insufficient supply");
+            let next := add(sload(totalSupply.slot), len)
+            if gt(next, sload(supply.slot)){
+                revert(0, 0) //TODO
+            }
         }
-        for (uint256 i = 0; i < addrs.length; i++) {
-            totalSupply++;
-            uint256 tokenId = totalSupply;
+
+        for (uint256 i = 0; i < len; i++) {
+            uint256 tokenId = totalSupply + i + 1;
             _mint(addrs[i], tokenId);
             sids[tokenId] = seriesIds[i];
         }
+        totalSupply += len;
     }
 
     /**
@@ -80,36 +105,26 @@ contract DeedGrainNFT is ERC721 {
      * Sets totalSupply for NFT.
      */
     function setSupply(uint256 supply_) external onlyControllers {
-        supply = supply_;
+        assembly{
+        // supply = supply_;
+            sstore(supply.slot, supply_)
+        }
     }
 
     /**
      * @dev See {IERC721Metadata-tokenURI}.
      */
     function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
     {
         require(tokenId <= totalSupply, "NFT does not exist");
         return string(abi.encodePacked(_baseMetadataURI, _uint2str(tokenId)));
     }
 
-    /**
-     * @dev See {IERC721Metadata-name}.
-     */
-    function name() public view override returns (string memory) {
-        return _deedgrainName;
-    }
-
-    /**
-     * @dev See {IERC721Metadata-symbol}.
-     */
-    function symbol() public view override returns (string memory) {
-        return _deedgrainSymbol;
-    }
 
     function _uint2str(uint256 _i) internal pure returns (string memory) {
         if (_i == 0) {
